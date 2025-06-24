bb20dc4ead4d981bf7ccd078f9a38993728c7e12
osvaldsoza <osvald.soza@gmail.com>
2025-06-24 16:47:51 -0300

    api gateway refactory

--- api-gateway/src/main/java/com/github/osvaldsoza/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/github/osvaldsoza/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.github.osvaldsoza.hrapigatewayzuul;
+package com.github.osvaldsoza.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class HrApiGatewayApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
