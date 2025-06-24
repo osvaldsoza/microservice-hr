8a23cc72137a3678e02b685677102184728e569a
osvaldsoza <osvald.soza@gmail.com>
2025-06-24 16:24:06 -0300

    api gateway implemented

--- hr-api-gateway/src/main/java/com/github/osvaldsoza/hrapigatewayzuul/HrApiGatewayApplication.java
+++ hr-api-gateway/src/main/java/com/github/osvaldsoza/hrapigatewayzuul/HrApiGatewayApplication.java
@@ -2,9 +2,10 @@ package com.github.osvaldsoza.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class HrApiGatewayApplication {
 
 	public static void main(String[] args) {
